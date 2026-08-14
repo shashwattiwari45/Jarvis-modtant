82dea01a681de0292a8229682e8566144165aff5
shashwattiwari45 <fastest2brothers@gmail.com>
2026-08-14 20:55:21 +0530

    Add Jarvis phone facade

---
+++ jarvis/phone.py
@@ -0,0 +1,7 @@
+"""Android/phone facade backed by jarvis.core."""
+from .core import (
+    open_app_on_phone, phone_volume, take_phone_screenshot, phone_key,
+    phone_battery, phone_info, type_on_phone, open_url_on_phone,
+    toggle_wifi, toggle_bluetooth, dial_number_on_phone, list_installed_apps_on_phone,
+    find_my_phone, play_youtube_on_phone, play_content_on_phone,
+)
